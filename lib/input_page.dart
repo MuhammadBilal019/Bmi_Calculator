c3bf6b8defac0340cf6347f695f9f2b19bcb72f4
MuhammadBilal019 <raobilalhameed@gmail.com>
2020-11-26 10:15:55 +0500

    Refactored Code Lec:57

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -12,52 +13,108 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(child: Row(
-            children: <Widget>[
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
+      body: Column(children: <Widget>[
+        Expanded(
+          child: Row(children: <Widget>[
+            Expanded(
+              child: new RepeatContainerCode(
+                colors: Color(0xFF1D1E33),
+                cardWidget: RepeatTextandIconWidget(
+                  icondata: FontAwesomeIcons.male,
+                  label: 'MALE',
                 ),
-              ),),
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
                 ),
-              ),),
-            ]
-          ),),
-          Expanded(child: Container(
+              ),
+            Expanded(
+              child: new RepeatContainerCode(
+              colors: Color(0xFF1D1E33),
+              cardWidget: RepeatTextandIconWidget(
+                icondata: FontAwesomeIcons.female,
+                label: 'FEMALE',
+              ),
+                ),
+              ),
+          ]),
+        ),
+        Expanded(
+          child: Container(
             margin: EdgeInsets.all(15.0),
             decoration: BoxDecoration(
               color: Color(0xFF1D1E33),
               borderRadius: BorderRadius.circular(10.0),
             ),
-          ),),
-          Expanded(child: Row(
-              children: <Widget>[
-                Expanded(child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                ),),
-                Expanded(child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                ),),
-              ]
-          ),),
-        ]
+          ),
+        ),
+        Expanded(
+          child: Row(children: <Widget>[
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ]),
+    );
+  }
+}
+
+class RepeatTextandIconWidget extends StatelessWidget {
+  RepeatTextandIconWidget({@required this.icondata,this.label});
+  final IconData icondata;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icondata,
+          size: 85.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 30.0,
+            color: Color(0xFF8D8E98),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class RepeatContainerCode extends StatelessWidget {
+  RepeatContainerCode({@required this.colors, this.cardWidget});
+
+  final Color colors;
+  final Widget cardWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      child: cardWidget,
+      decoration: BoxDecoration(
+        color: colors,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
